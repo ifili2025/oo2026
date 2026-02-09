36aec108625e712f81d8dc609b77b3f464bd72c6
Igor Filippov <ifili@tlu.ee>
2026-02-09 22:36:14 +0200

    Add files via upload
    Funktsioon arvutab sisestatud meeskonna mängude statistika tulemuste põhjal. Punktid kodumängudes ja võõrsilmängudes eraldi, maksimaalne võimalik punktide arv mängitud mängude eest.

---
+++ 01_funktsioon/1kodutoo.py
@@ -0,0 +1,30 @@
+import urllib.request
+
+fsisu = urllib.request.urlopen('https://oop2026.igorfilippov.eu/1kodutoo.txt').read().decode("utf-8")
+andmeread = fsisu.split("\n")[1:] 
+andmeread = [rida.strip().split(",") for rida in andmeread if rida.strip()]
+def meeskonna_punktid(team):
+    home_point = 0
+    away_point = 0
+    game_count = 0
+    for rida in andmeread:
+        if rida[0] == team:
+            game_count += 1
+            if int(rida[2]) > int(rida[3]):
+                home_point += 3
+            elif int(rida[2]) == int(rida[3]):
+                home_point += 1
+        if rida[1] == team:
+            game_count += 1
+            if int(rida[3]) > int(rida[2]):
+                away_point += 3
+            elif int(rida[3]) == int(rida[2]):
+                away_point += 1            
+    return game_count,home_point, away_point
+
+team = str(input('Sisesta meeskonna nimi, annan sulle teada selle meeskonna punkti praegusel hooajal summa: '))
+game_count, home, away = meeskonna_punktid(team)
+print(team, "sellel hooajal omab", home + away, "punktid, nendest tuli", home, "punkti kodumängudest ja", away, "punkti välimängudest ja kokku mängiti", game_count, "mängu. Max võimalik punktide arv võiks olla", game_count * 3)
+
+
+
